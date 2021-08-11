e5d4e0f2d28c5ea733248b0ff9afcd198ccefbbf
Albino Cianciotti <albino.cianciotti@protonmail.com>
2021-08-11 17:39:10 +0200

    Light and dark themes, developed start deploy action

--- functions.py
+++ functions.py
@@ -2,9 +2,3 @@ from PyQt5.QtWidgets import QListWidgetItem
 
 
 
-def generateDeployListItems(ui):
-    for i in range(10):
-        item = QListWidgetItem("Item %i" % i)
-        ui.listWidget_deploys.addItem(item)
-
-
